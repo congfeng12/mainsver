b24e605fc08d75fc503d1a990709ee2c3bfe02d0
C-Maple <congfeng12@163.com>
2021-07-22 15:42:19 +0800

    初始化后端程序

---
+++ src/main/java/cn/cmaple/mainsver/MainsverApplication.java
@@ -0,0 +1,13 @@
+package cn.cmaple.mainsver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainsverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainsverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/cmaple/mainsver/MainsverApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cmaple.mainsver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainsverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
