377e1458bd4541f8071732876d327cfa4bf009a2
C-Maple <congfeng12@163.com>
2021-07-23 15:06:50 +0800

    新增配置文件内容，增加第三方包引入，增加基础服务类

--- src/main/java/cn/cmaple/mainsver/MainsverApplication.java
+++ src/main/java/cn/cmaple/mainsver/MainsverApplication.java
@@ -1,9 +1,12 @@
 package cn.cmaple.mainsver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@MapperScan(basePackages = "cn.cmaple.mainsver.mapper")
 public class MainsverApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/cn/cmaple/mainsver/controller/BasicController.java
@@ -0,0 +1,39 @@
+package cn.cmaple.mainsver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName: BasicController
+ * @Description: 基础服务类，用于处理官网相关无需登陆的相关服务
+ * @author: CMAPLE
+ * @date 2021/7/23 下午1:32
+ * @Version 1.0
+ * @since JDK1.8 及 更高
+ */
+@RestController
+@RequestMapping("/Basic")
+public class BasicController {
+
+    /**
+     * @Title: HeadlineNews
+     * @Description 主页头条位置信息获取函数，获取内容为：图片链接、标题文字、详细内容链接地址、返回结果标记、返回结果说明
+     * @return Map<String, Object>
+     * @throws null
+     * @author CMAPLE
+     * @date 2021/7/23 下午1:51
+     */
+    @RequestMapping(value = "/headline", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public Map<String, Object> HeadlineNews(){
+        //返回参数初始化
+        Map<String, Object> map = new HashMap<String, Object>();
+
+
+
+        return map;
+    }
+}
